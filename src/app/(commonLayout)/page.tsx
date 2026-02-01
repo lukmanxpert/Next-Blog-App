5d11f09a6618b682c604137fb4a49a6ce7d4ad11
lukmanxpert <lukmanmiah2004@gmail.com>
2026-02-01 23:55:21 +0600

    ...

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const { data: session } = await authClient.getSession();
-  console.log("session :>> ", session);
+  const cookieStore = await cookies();
+  const res = await fetch("http://localhost:5000/api/auth/get-session", {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store"
+  });
+  console.log("response session data :>> ", await res.json());
   return (
     <div>
       <h1>This is home page.</h1>
